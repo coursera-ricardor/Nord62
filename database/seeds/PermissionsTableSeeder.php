4b04db22aa5a78423dd36ba767e19906e35f998b
Ricardo Rodriguez <coursera-ricardor@users.noreply.github.com>
2019-09-12 10:45:57 +0200

    DB Seeding with try catch structures

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
+// use Spatie\Permission\Models\Permission;
 
 class PermissionsTableSeeder extends Seeder
 {
@@ -19,34 +19,68 @@ class PermissionsTableSeeder extends Seeder
 				Web access rights
 				System functionality rights 
 		*/
+        $Table = 'permissions';
+        $Records = [];
+
 		/*
 			Web functionality
 			Assigned to the User
 				Each User has a Profile
 		*/
 		// Permission::create(['guard_name' => 'web', 'name' => 'system_manage']);
-		Permission::create(['name' => 'system_manage']);
-		Permission::create(['name' => 'users_manage']);
-		Permission::create(['name' => 'browse']);
+		// Permission::create(['name' => 'system_manage']);
+        $Records[] = ['name' => 'users_manage', 'description' => 'Manage Users']; 
+		$Records[] = ['name' => 'browse','guard_name' => 'web'];
 		
 		/*
 			System functionality (Multi-guard maybe will be discontinued)
 			Assigned to the Profile
 		*/
 		// Permission::create(['guard_name' => 'users', 'name' => 'catalogs_admin']);
-		Permission::create(['name' => 'catalogs_admin']);
-		Permission::create(['name' => 'catalogs_browse']);
-		Permission::create(['name' => 'catalogs_create']);
-		Permission::create(['name' => 'catalogs_edit']);
+		$Records[] = ['name' => 'catalogs_admin'];
+		$Records[] = ['name' => 'catalogs_browse'];
+		$Records[] = ['name' => 'catalogs_create'];
+		$Records[] = ['name' => 'catalogs_edit'];
 		
-		Permission::create(['name' => 'Countrycodes_admin']);
-		Permission::create(['name' => 'Countrycodes_browse']);
-		Permission::create(['name' => 'Countrycodes_create']);
-		Permission::create(['name' => 'Countrycodes_edit']);
+		$Records[] = ['name' => 'Countrycodes_admin'];
+		$Records[] = ['name' => 'Countrycodes_browse'];
+		$Records[] = ['name' => 'Countrycodes_create'];
+		$Records[] = ['name' => 'Countrycodes_edit'];
 		
-		Permission::create(['name' => 'Language_codes_admin']);
-		Permission::create(['name' => 'Language_codes_browse']);
-		Permission::create(['name' => 'Language_codes_create']);
-		Permission::create(['name' => 'Language_codes_edit']);
+		$Records[] = ['name' => 'Language_codes_admin'];
+		$Records[] = ['name' => 'Language_codes_browse'];
+		$Records[] = ['name' => 'Language_codes_create'];
+		$Records[] = ['name' => 'Language_codes_edit'];
+
+        // Create the records
+        //  Note: If the record exists or it is a problem with the creation, the seeder won't fail.
+        //      In debug mode uncomment the error message echo
+        $this->createRecordClass(Spatie\Permission\Models\Permission::class, $Records);
+
     }
+
+    /**
+     * Record Creation using an array with the information.
+     * The information could be uploaded from other sources
+     *
+     * @param  class  $dbModel
+     * @param  array  $dbRecords
+    */
+    private function createRecordClass( $dbModel, $dbRecords) {
+        foreach ($dbRecords as $dbRecord) {
+            try {
+                $dbModel::create($dbRecord);
+            }
+            // @todo: Include the QueryException to allow to continue the excecution
+            catch ( \Illuminate\Database\QueryException $ex) {
+                // echo $ex->getMessage();
+            }
+            catch ( Spatie\Permission\Exceptions\PermissionAlreadyExists $ex) {
+                // echo $ex->getMessage();
+                
+            }
+        }
+    
+    }
+
 }
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -16,32 +16,261 @@ class RolesTableSeeder extends Seeder
         /*
 			Web Roles
 		*/
+        // Creation of Primary Roles
 		// $role = Role::create(['guard_name' => 'web', 'name' => 'administrator']);
         if ( ! Role::whereName('Administrator')->exists()) {
-    		Role::create(['name' => 'Administrator']);    
-        }
-        $role->givePermissionTo('browse');
-        $role->givePermissionTo('catalogs_browse');
-        $role->givePermissionTo('catalogs_admin');
-        $role->givePermissionTo('users_manage');
+    		$role = Role::create(['name' => 'Administrator']);
 
+            // Assign permissions to the Role
+            $role->givePermissionTo('browse');
+            $role->givePermissionTo('catalogs_browse');
+            $role->givePermissionTo('catalogs_admin');
+            $role->givePermissionTo('users_manage');
+        }
+
+        /*
+            Example: Assigning a Role with a non existing Permission
+            It is possible to implement an automatic creation of the non existent Permission
+        */
+        /*
+        $role = Role::whereName('Administrator')->firstOrFail();
+        try  {
+            $role->givePermissionTo('No Existe browse');
+        } catch ( Spatie\Permission\Exceptions\PermissionDoesNotExist $ex) {
+            echo "Permission: "."No Existe browse "." doesn't exist" . "\n";        
+        }
+        try  {
+            $role->givePermissionTo('browse');
+        } catch ( Spatie\Permission\Exceptions\PermissionDoesNotExist $ex) {
+            echo "Permission: "."browse"." doesn't exist" . "\n";        
+        }
+        try  {
+            $role->givePermissionTo('No Existe browse 2');
+        } catch ( Spatie\Permission\Exceptions\PermissionDoesNotExist $ex) {
+            echo "Permission: "."No Existe browse 2"." doesn't exist" . "\n";        
+        }
+        */
+
+        //
+        // Creation of Secondary Roles
 		// $role = Role::create(['guard_name' => 'web','name' => 'user']);
-		$role = Role::create(['name' => 'user']);
-        $role->givePermissionTo('browse');
-        $role->givePermissionTo('catalogs_browse');
+        //
+        $Table = 'roles';
+        $Records = [];
+
+        //  $role = Role::create(['name' => 'user']);
+        //      $role->givePermissionTo('browse');
+        //      $role->givePermissionTo('catalogs_browse');
+        $Records[] = [
+            'name' => 'User',
+            // 'guard_name' => 'web' ,
+            // 'description' => 'User',
+            'permissions' => ['browse', 'catalogs_browse','browsing'],
+            // 'planets' => ['mercury', 'venus','earth','mars'],
+        ]; 
+
 
 		// $role = Role::create(['guard_name' => 'web', 'name' => 'guest']);
-		$role = Role::create(['name' => 'guest']);
-        $role->givePermissionTo('browse');
+		// $role = Role::create(['name' => 'guest']);
+        // $role->givePermissionTo('browse');
+        $Records[] = [
+            'name' => 'Guest',
+            'guard_name' => 'web' ,
+            'description' => 'Guest User',
+            'permissions' => ['browse']
+        ]; 
 		
 		/*
-			System Roles
+		 *	System Roles
 		*/
-		// $role = Role::create(['guard_name' => 'users','name' => 'administrator']);
+        /*
 		$role = Role::create(['name' => 'User Administrator']);
         $role->givePermissionTo('catalogs_admin');
         $role->givePermissionTo('users_manage');
         $role->givePermissionTo('browse');
+        */
+        $Records[] = [
+            'name' => 'User Administrator',
+            'guard_name' => 'web' ,
+            'description' => 'Guest User',
+            'permissions' => ['catalogs_admim', 'users_manage','browse']
+        ]; 
+
+        // Roles Creation
+        // $this->createRecordClass(Spatie\Permission\Models\Role::class, $Records);
+        $this->createRecordClass(Spatie\Permission\Models\Role::class, $Records,['permissions','planets']);
 
     }
+
+    /**
+     * Record Creation using an array with the information.
+     * The information could be uploaded from other sources
+     * Note:
+     *      This function is NOT recursive, Details of Detail records are NOT processed.
+     *      i.e: user->Role->Permissions
+     *      The function wil Process structure like
+     *      roles->permissions
+     *      user->shipAddress (Not applicable to this Seeder)
+     *
+     * @param  class  $dbModel
+     * @param  array  $dbRecords
+     * @param  array  $dbDetailRecords
+    */
+    private function createRecordClass( $dbModel, $dbRecords, $dbDetailRecords = []) {
+        foreach ($dbRecords as $dbRecord) {
+            // var_dump($dbRecord);
+            echo 'Nombre: ' . $dbRecord["name"]  . "\n";
+
+            /*
+                Version Create - Find
+            */
+            try {
+
+                $newModel = $dbModel::create(collect($dbRecord)->except($dbDetailRecords)->toArray());
+
+            } catch (Spatie\Permission\Exceptions\RoleAlreadyExists $ex) {
+                echo $ex->getMessage();
+                $newModel = $dbModel::findByName($dbRecord["name"]);
+                // dd(var_dump($newModel));
+            }
+            catch (Exception $ex) {
+                echo $ex->getMessage() . "\n";
+            } finally {
+                // dd(var_dump($newModel));
+                /*
+                    * Process Detail Records set
+                    * 
+                */
+                if (isset($newModel)) {
+                    // dd(var_dump($newModel));
+                    echo "Role a procesar: " . $newModel->name  . "\n";
+
+                    foreach ($dbDetailRecords as $dbDetailRecord) {
+                        echo var_dump($dbDetailRecord);
+                        if ( isset($dbRecord[$dbDetailRecord]) ) {
+                            // var_dump($dbRecord[$dbDetailRecord]);
+
+                            foreach ($dbRecord[$dbDetailRecord] as $dbDetail) {
+                                echo "Assigning: " . $dbDetail . "\n";
+
+                                try {
+                                    $newModel->givePermissionTo($dbDetail);
+                                } catch ( Spatie\Permission\Exceptions\PermissionDoesNotExist $ex) {
+                                    echo $ex->getMessage() . "\n";                                
+                                } catch ( Exception $ex) {
+                                    echo $ex->getMessage() . "\n";
+                                }
+
+                            }
+
+                        }                
+                    }
+                }
+            }
+
+
+            /*
+                Version Find - Create
+                Initial Seed most probable Create-Find is more appropiate
+            */
+            /*
+            try {
+                $newModel = $dbModel::findByName($dbRecord["name"]);
+            } catch (Exception $ex) {
+                dd($ex->getMessage());
+
+                $newModel = $dbModel::create(collect($dbRecord)->except($dbDetailRecords)->toArray());
+            }
+            */
+
+        }
+    }
+
+    /**
+     * Record Creation using an array with the information.
+     * The information could be uploaded from other sources
+     * Note:
+     *      This function is NOT recursive, Details of Detail records are NOT processed.
+     *      i.e: user->Role->Permissions
+     *      The function wil Process structure like
+     *      roles->permissions
+     *      user->shipAddress (Not applicable to this Seeder)
+     *
+     * @param  class  $dbModel
+     * @param  array  $dbRecords
+     * @param  array  $dbDetailRecords
+    */
+    private function createRecordClassInitialTest( $dbModel, $dbRecords, $dbDetailRecords = []) {
+        foreach ($dbRecords as $dbRecord) {
+            // var_dump($dbRecord);
+            echo 'Nombre: ' . $dbRecord["name"]  . "\n";
+
+            /*
+                Version Create - Find
+            */
+            try {
+            
+                /*
+                 * Using collections will allow to have access to multiple methods
+                 * https://laravel.com/docs/6.0/collections
+                */
+                // dd(collect($dbRecord)->except(['permissions']));
+                // dd(collect($dbRecord)->except($dbDetailRecords));
+
+                // This will produce and error if the key 'permissions' (Detail records) is included 
+                // $newModel = $dbModel::create($dbRecord);
+                // $newModel = $dbModel::create(collect($dbRecord)->except(['permissions'])->toArray());
+                $newModel = $dbModel::create(collect($dbRecord)->except($dbDetailRecords)->toArray());
+
+                /*
+                 * Process Detail Records set
+                 * 
+                */
+                /*
+                //
+                // Single Detail reference Records
+                //
+                if ( isset($dbRecord['permissions']) ) {
+                    foreach ($dbRecord['permissions'] as $dbDetail) {
+                        // var_dump($dbDetail);
+                        echo 'Permission: ' . $dbDetail . "\n";
+                    }                
+                }
+                */
+                //
+                // Multiple Detail reference Records
+                //
+                // dd($dbDetailRecords);
+                foreach ($dbDetailRecords as $dbDetailRecord) {
+                    echo var_dump($dbDetailRecord);
+                    if ( isset($dbRecord[$dbDetailRecord]) ) {
+                        // var_dump($dbRecord[$dbDetailRecord]);
+
+                        foreach ($dbRecord[$dbDetailRecord] as $dbDetail) {
+                            var_dump($dbDetail);
+                            // echo 'Permission: ' . $dbDetail . "\n";
+                        }
+                    }                
+                }
+            }
+            catch (Exception $ex) {
+                dd('stop here');
+            
+            }
+            // @todo: Include the QueryException to allow to continue the excecution
+            catch ( \Illuminate\Database\QueryException $ex) {
+                echo $ex->getMessage();
+            }
+            catch ( Spatie\Permission\Exceptions\RoleAlreadyExists $ex) {
+                echo $ex->getMessage();
+                dd(var_dump($dbRecord));
+            }
+            finally {
+                dd('After Error' . var_dump($newModel));
+            
+            }
+        }
+    
+    }
+
 }
